cffce20676d50c17464ad7307c0cd438ba9319f7
Tobias R. <no@thankyou.com>
2026-02-05 08:39:17 +0100

    Completed implementation of logic

--- DTP_Case_Plukliste_ConsoleApp/Program.cs
+++ DTP_Case_Plukliste_ConsoleApp/Program.cs
@@ -1,4 +1,5 @@
 ﻿//Eksempel på funktionel kodning hvor der kun bliver brugt et model lag
+using System.Linq.Expressions;
 using System.Xml.Serialization;
 namespace Plukliste
 
@@ -15,91 +16,116 @@ namespace Plukliste
             if (!Directory.Exists("export"))
             {
                 Console.WriteLine("Folder not found.");
-                Console.WriteLine(Directory.GetCurrentDirectory()+"/export");
+                Console.WriteLine(Directory.GetCurrentDirectory() + "/export");
                 return;
             }
             files = Directory.EnumerateFiles("export").ToList();
 
-            //ACT
+        //ACT
+
+        InitFile:
+            Console.Clear();
+            if (files.Count == 0)
+            {
+                Console.WriteLine("No files found.");
+            }
+            else
+            {
+                Console.WriteLine($"Plukliste {index + 1} af {files.Count}");
+                Console.WriteLine($"file: {files[index]}");
+
+                FileStream file = File.OpenRead(files[index]);
+                XmlSerializer xmlSerializer = new XmlSerializer(typeof(Pluklist));
+                var plukliste = (Pluklist?)xmlSerializer.Deserialize(file);
+
+                if (plukliste != null && plukliste.Lines != null)
+                {
+                    Console.WriteLine("\n{0, -13}{1}", "Name:", plukliste.Name);
+                    Console.WriteLine("{0, -13}{1}", "Forsendelse:", plukliste.Forsendelse);
+                    //TODO: Add adresse to screen print
+
+                    Console.WriteLine("\n{0,-7}{1,-9}{2,-20}{3}", "Antal", "Type", "Produktnr.", "Navn");
+                    foreach (var item in plukliste.Lines)
+                    {
+                        Console.WriteLine("{0,-7}{1,-9}{2,-20}{3}", item.Amount, item.Type, item.ProductID, item.Title);
+                    }
+                }
+                file.Close();
+                Console.WriteLine("\n\nOptions:");
+            }
+            //Print options
+            int optionSelected = 0;
             bool isConfirmed = false;
+            string cursor = "<-";
+            (int right, int top) = Console.GetCursorPosition();
+            List<string> options = new List<string>
+            {
+                "Forrige plukseddel",
+                "Naeste plukseddel",
+                "Genindlaes pluksedler",
+                "Afslut plukseddel",
+                "Quit"
+            };
+            if (index >= 0 == false)
+            {
+                options.Remove("Afslut plukseddel");
+            }
+            if (index > 0 == false)
+            {
+                options.Remove("Forrige plukseddel");
+            }
+            if (index < files.Count - 1 == false)
+            {
+                options.Remove("Naeste plukseddel");
+            }
             while (!isConfirmed)
             {
-            InitFile:
-                if (files.Count == 0)
-                {
-                    Console.WriteLine("No files found.");
-                }
-                else
-                {
-                    Console.WriteLine($"Plukliste {index + 1} af {files.Count}");
-                    Console.WriteLine($"file: {files[index]}");
-
-                    FileStream file = File.OpenRead(files[index]);
-                    XmlSerializer xmlSerializer = new XmlSerializer(typeof(Pluklist));
-                    var plukliste = (Pluklist?)xmlSerializer.Deserialize(file);
-
-                    if (plukliste != null && plukliste.Lines != null)
-                    {
-                        Console.WriteLine("\n{0, -13}{1}", "Name:", plukliste.Name);
-                        Console.WriteLine("{0, -13}{1}", "Forsendelse:", plukliste.Forsendelse);
-                        //TODO: Add adresse to screen print
-
-                        Console.WriteLine("\n{0,-7}{1,-9}{2,-20}{3}", "Antal", "Type", "Produktnr.", "Navn");
-                        foreach (var item in plukliste.Lines)
-                        {
-                            Console.WriteLine("{0,-7}{1,-9}{2,-20}{3}", item.Amount, item.Type, item.ProductID, item.Title);
-                        }
-                    }
-                    file.Close();
-                    Console.WriteLine("\n\nOptions:");
-                }
-                //Print options
-                int option = 0;
-                int optionsCount = 0;
-                isConfirmed = false;
-                string cursor = "<-";
-                (int right, int top) = Console.GetCursorPosition();
                 Console.SetCursorPosition(right, top);
-                Console.WriteLine($"Quit{(option == 0 ? cursor : "  ")}");
-                Console.WriteLine($"Afslut plukseddel{(option == 1 ? cursor : "  ")}");
-                if (index > 0)
+
+                for (int i = 0; i < options.Count; i++)
                 {
-                    Console.WriteLine($"Forrige plukseddel{(option == 2 ? cursor : "  ")}");
+                    Console.WriteLine(options[i] + $"{(optionSelected == options.IndexOf(options[i]) ? cursor : "  ")}");
                 }
-                if (index < files.Count - 1)
-                {
-                    Console.WriteLine($"Næste plukseddel{(option == 3 ? cursor : "  ")}");
-                }
-                Console.WriteLine($"Genindlæs pluksedler{(option == 4 ? cursor : "  ")}");
                 key = Console.ReadKey(true);
 
                 switch (key.Key)
                 {
                     case ConsoleKey.DownArrow:
-                        option = (option == 4 ? 0 : option + 1);
+                        optionSelected = (optionSelected == options.Count - 1 ? 0 : optionSelected + 1);
                         break;
                     case ConsoleKey.UpArrow:
-                        option = (option == 0 ? 4 : option - 1);
+                        optionSelected = (optionSelected == 0 ? options.Count - 1 : optionSelected - 1);
                         break;
                     case ConsoleKey.Enter:
-                        switch (option)
-                        {
-                            case 0:
-                                isConfirmed = true;
-                                break;
-                            case 1:
-                                var filewithoutPath = files[index].Substring(files[index].LastIndexOf('\\'));
-                                File.Move(files[index], string.Format(@"import\\{0}", filewithoutPath));
-                                Console.WriteLine($"Plukseddel {files[index]} afsluttet.");
-                                files.Remove(files[index]);
-                                if (index == files.Count) index--;
-                                goto InitFile;
-                                break;
-
-
-                        }
+                        isConfirmed = true;
                         break;
                 }
+                Console.SetCursorPosition(right, top);
+            }
+            switch (options[optionSelected])
+            {
+                case "Forrige plukseddel":
+                    if (index > 0) index--;
+                    goto InitFile;
+                case "Naeste plukseddel":
+                    if (index < files.Count - 1) index++;
+                    goto InitFile;
+                case "Genindlaes pluksedler":
+                    files = Directory.EnumerateFiles("export").ToList();
+                    index = 0;
+                    Console.WriteLine("Pluklister genindlæst");
+                    goto InitFile;
+                case "Afslut plukseddel":
+                    //Move files to import directory
+                    var filewithoutPath = files[index].Substring(files[index].LastIndexOf('\\'));
+                    File.Move(files[index], string.Format(@"import\\{0}", filewithoutPath));
+                    Console.WriteLine($"Plukseddel {files[index]} afsluttet.");
+                    files.Remove(files[index]);
+                    if (index == files.Count) index--;
+                    goto InitFile;
+                case "Quit":
+                    Directory.Delete("import", true);
+                    return;
             }
         }
     }
